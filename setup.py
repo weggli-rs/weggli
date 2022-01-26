59ba3974043acaa35d31dfd0b5fcfda15e83f2cd
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2022-01-26 18:33:38 +0100

    python: Use setuptools to build python library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="weggli",
+    version="0.2.3",
+    author="fwilhelm",
+    url="https://github.com/googleprojectzero/weggli",
+    rust_extensions=[RustExtension("weggli", binding=Binding.PyO3, features=["python"])],
+    zip_safe=False,
+)
