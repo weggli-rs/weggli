ad8d424e0da5b65f72ce69a884f1cd95f596d85f
Felix Wilhelm <fwilhelm@google.com>
2022-03-21 11:47:38 +0100

    v0.2.4 release
    Changes since v0.2.3:
    - Grammar improvements
    - Better C++ parsing
    - Fix panic during result printing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="weggli",
-    version="0.2.3",
+    version="0.2.4",
     author="fwilhelm",
     url="https://github.com/googleprojectzero/weggli",
     rust_extensions=[RustExtension("weggli", binding=Binding.PyO3, features=["python"])],
